e94a3770cb7acad659403d3f2241062900aa3717
DayYoung <machuanjia@laiye.com>
2021-10-28 13:56:34 +0800

    fix: release

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-08 19:46:32
- * @LastEditTime: 2021-10-27 15:49:40
+ * @LastEditTime: 2021-10-28 13:56:30
  * @FilePath: /doc-test/src/index.ts
  * @Description:
  */
