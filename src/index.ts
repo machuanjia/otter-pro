0e1ba08baa82bc28d4e7b9f22c2042231fd721e6
DayYoung <machuanjia@laiye.com>
2021-11-08 14:41:32 +0800

    feat: change IList

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-10-08 19:46:32
- * @LastEditTime: 2021-11-08 13:58:14
+ * @LastEditTime: 2021-11-08 14:40:51
  * @FilePath: /doc-test/src/index.ts
  * @Description:
  */
--- src/models/IList.ts
+++ src/models/IList.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-11-08 13:52:39
- * @LastEditTime: 2021-11-08 13:54:48
+ * @LastEditTime: 2021-11-08 14:40:41
  * @FilePath: /doc-test/src/models/IList.ts
  * @Description:
  */
 export interface IList {
-  count: number;
+  total: number;
   page_size: number;
   page_index: number;
   value: any;
