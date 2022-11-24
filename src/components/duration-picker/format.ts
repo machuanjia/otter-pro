0489dd89d113cac8056a652a8907f0b16f3365ac
lubenben <lubenben@laiye.com>
2022-11-24 15:28:29 +0800

    feat: Navtabs

--- src/components/duration-picker/format.ts
+++ src/components/duration-picker/format.ts
@@ -1,12 +1,22 @@
-import moment from 'moment';
+/*
+ * @Author: lubenben
+ * @Date: 2022-11-22 15:32:45
+ * @LastEditors: lubenben
+ * @LastEditTime: 2022-11-24 10:07:00
+ */
+import dayjs from 'dayjs';
 
 export const getTimeInterval = (time: string) => {
-    // 当前月的1号毫秒数 + 传入的秒数 => 转换成日期
-    const timestamp = Number(moment('1 00:00:00', 'DD HH:mm:ss').format('X') + '000') + Number(time + '000')
-    return moment(timestamp)
-}
+  // 当前月的1号毫秒数 + 传入的秒数 => 转换成日期
+  const timestamp =
+    Number(dayjs('1 00:00:00', 'DD HH:mm:ss').format('X') + '000') + Number(time + '000');
+  return dayjs(timestamp);
+};
 
-export const getTimeString = (time: moment.Moment) => {
-    // 给定时间 - 当前月的1号毫秒数 = 秒数间隔
-    return Number(time.format('X')) - Number(moment(time.format('YYYYMM')+'1 00:00:00', 'YYYYMMD HH:mm:ss').format('X'))
-}
+export const getTimeString = (time: any) => {
+  // 给定时间 - 当前月的1号毫秒数 = 秒数间隔
+  return (
+    Number(time.format('X')) -
+    Number(dayjs(time.format('YYYYMM') + '1 00:00:00', 'YYYYMMD HH:mm:ss').format('X'))
+  );
+};
--- src/components/nav-tabs/index.tsx
+++ src/components/nav-tabs/index.tsx
@@ -1,11 +1,14 @@
 /*
  * @Author: D.Y.M
  * @Date: 2021-11-16 15:33:19
- * @LastEditTime: 2021-12-25 15:30:51
+ * @LastEditTime: 2022-11-24 14:53:10
  * @FilePath: /otter-pro/src/components/nav-tabs/index.tsx
  * @Description:
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { CaretDownOutlined } from '@ant-design/icons';
+import { Dropdown, Menu, Tooltip } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { LIB_PREFIX } from '../../constants';
@@ -21,41 +24,108 @@ type IProps = {
   active: string;
   size?: string;
   align?: string;
+  maxLength?: number;
   onClick?: (key: string) => void;
 };
-const TabLink = ({ item, atv, size }: any) => {
-  return (
-    <Link to={item.path} key={item.path}>
+
+const TabLink = ({ item, atv, size }: any) => (
+  <>
+    {item.name.length > 5 ? (
+      <Tooltip title={item.name}>
+        <Link to={item.path} key={item.path}>
+          <span
+            className={`${LIB_PREFIX}-nav-tab ${LIB_PREFIX}-nav-tab-${size} ${
+              atv === item.key && `${LIB_PREFIX}-nav-tab-active`
+            }`}
+          >
+            {item.name}
+          </span>
+        </Link>
+      </Tooltip>
+    ) : (
+      <Link to={item.path} key={item.path}>
+        <span
+          className={`${LIB_PREFIX}-nav-tab ${LIB_PREFIX}-nav-tab-${size} ${
+            atv === item.key && `${LIB_PREFIX}-nav-tab-active`
+          }`}
+        >
+          {item.name}
+        </span>
+      </Link>
+    )}
+  </>
+);
+const Tab = ({ item, atv, size, onClick }: any) => (
+  <>
+    {item.name.length > 5 ? (
+      <Tooltip title={item.name}>
+        <span
+          onClick={() => {
+            onClick && onClick(item.key);
+          }}
+          className={`${LIB_PREFIX}-nav-tab ${LIB_PREFIX}-nav-tab-${size} ${
+            atv === item.key && `${LIB_PREFIX}-nav-tab-active`
+          }`}
+        >
+          {item.name}
+        </span>
+      </Tooltip>
+    ) : (
       <span
+        onClick={() => {
+          onClick && onClick(item.key);
+        }}
         className={`${LIB_PREFIX}-nav-tab ${LIB_PREFIX}-nav-tab-${size} ${
           atv === item.key && `${LIB_PREFIX}-nav-tab-active`
         }`}
       >
         {item.name}
       </span>
-    </Link>
-  );
-};
-const Tab = ({ item, atv, size, onClick }: any) => {
-  return (
-    <span
-      onClick={() => {
-        onClick && onClick(item.key);
-      }}
-      className={`${LIB_PREFIX}-nav-tab ${LIB_PREFIX}-nav-tab-${size} ${
-        atv === item.key && `${LIB_PREFIX}-nav-tab-active`
-      }`}
-    >
-      {item.name}
-    </span>
-  );
-};
+    )}
+  </>
+);
+
 const NavTabs = (props: IProps) => {
-  const { tabs, active, className = '', size = '', align = 'center', onClick } = props;
+  const {
+    tabs = [],
+    active,
+    className = '',
+    maxLength = 0,
+    size = '',
+    align = 'center',
+    onClick,
+  } = props;
+
+  const [data, setData] = useState<any>([]);
+  const [menuData, setMenuData] = useState<any>([]);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const handleData = () => {
+    if (maxLength && tabs.length > maxLength) {
+      const data: any[] = [];
+      const menuData: any[] = [];
+      tabs.forEach((item, index) => (index > maxLength ? menuData.push(item) : data.push(item)));
+
+      setData(data);
+      setMenuData(menuData);
+      setIsMenuVisible(true);
+    } else {
+      setData(tabs);
+      setMenuData([]);
+      setIsMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    tabs.length && handleData();
+  }, [tabs, maxLength]);
+
   return (
-    <div className={`${LIB_PREFIX}-nav-tabs  ${LIB_PREFIX}-nav-tabs-${align} ${className}`}>
-      {tabs.map((n: INav) => {
-        return (
+    <div className=" flex">
+      <div
+        className={`${LIB_PREFIX}-nav-tabs ${LIB_PREFIX}-nav-tabs-${size} ${LIB_PREFIX}-nav-tabs-${align} ${className}`}
+      >
+        {data.map((n: INav) => (
           <>
             {n.path ? (
               <TabLink item={n} atv={active} size={size} />
@@ -63,8 +133,26 @@ const NavTabs = (props: IProps) => {
               <Tab atv={active} item={n} size={size} onClick={onClick} />
             )}
           </>
-        );
-      })}
+        ))}
+      </div>
+      {isMenuVisible && (
+        <Dropdown
+          overlay={
+            <Menu
+              items={menuData.map(({ name, key }: any) => ({
+                label: name,
+                key,
+              }))}
+              selectedKeys={[active]}
+              onClick={({ key }) => onClick && onClick(key)}
+            />
+          }
+        >
+          <CaretDownOutlined
+            className={`${LIB_PREFIX}-nav-tabs-icon ${LIB_PREFIX}-nav-tabs-icon-${size}`}
+          />
+        </Dropdown>
+      )}
     </div>
   );
 };
