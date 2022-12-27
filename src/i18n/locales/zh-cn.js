0baa68f46f3f334ec9cbcfdc7076a0c17572b30e
machuanjia <yanshi0429@126.com>
2022-12-27 09:08:00 +0800

    fix: i18n

--- src/i18n/locales/zh-cn.js
+++ src/i18n/locales/zh-cn.js
@@ -6,50 +6,75 @@
  * @Description:
  */
 
-export default  {
-  "routes": {
-    "login": "登录",
-    "logout": "退出",
-    "project": "项目",
-    "team": "团队",
-    "redirect": "跳转",
-    "main": "主页",
-    "dashboard": "概览",
-    "data": "数据",
-    "dataLake": "数据湖",
-    "dataSet": "数据集",
-    "dataTask": "标注任务",
-    "dataSynthesis": "数据合成"
+export default {
+  common: {
+    routes: {
+      login: '登录',
+      logout: '退出',
+      project: '项目',
+      team: '团队',
+      main: '主页',
+      engines: '引擎管理',
+      capabilities: 'AI能力管理',
+      basicInfo: '基础信息',
+      datasetTags: '数据集标签',
+      annotationTemplates: '标注模板',
+      defaultDatasets: '默认数据集',
+      dashboard: '概览',
+      data: '数据',
+      dataLake: '数据湖',
+      dataSet: '数据集',
+      dataTask: '标注任务',
+      dataSynthesis: '数据合成',
+      annotation: '标注',
+      autoCheck: '自动校对',
+      experiment: '实验',
+      trainingTask: '训练任务',
+      viewTrainingTask: '查看训练任务',
+      model: '模型',
+      modelRepos: '模型仓库',
+      modelReposVersion: '模型版本',
+      test: '测试',
+      testingTask: '测试任务',
+      trace: 'Trace',
+      testingTaskAnalyze: '测试任务效果分析',
+      testingDatasetAnalyze: '数据集效果分析',
+      testingDataAnalyze: '数据效果分析',
+      badcase: 'Badcase',
+      publish: '发布',
+      percipience: '洞察',
+      settingCenter: '配置中心',
+      aiEngine: 'AI 引擎',
+      aiCapability: 'AI 能力',
+    },
+    actions: {
+      create: '新建',
+      update: '编辑',
+      delete: '删除',
+      cancel: '取消',
+      ok: '确定',
+      view: '查看',
+    },
+    list: {
+      label: '{{name}}列表',
+      total: '共{{total}}条',
+      title: '名称',
+      status: '状态',
+      description: '描述',
+      createTime: '创建时间',
+      creator: '创建者',
+      actions: '操作',
+    },
+    placeholder: {
+      title: '请输入名称',
+      description: '请输入描述',
+      searchText: '请您输入...',
+    },
+    dir: {
+      root: '根目录',
+      testing: '测试集',
+      validation: '验证集',
+      training: '训练集',
+    },
   },
-  "common": {
-    "actions": {
-      "create": "新建",
-      "update": "编辑",
-      "delete": "删除",
-      "cancel": "取消",
-      "ok": "确定",
-      "view": "查看"
-    },
-    "list": {
-      "label": "{{name}}列表",
-      "total": "共{{total}}条",
-      "title": "名称",
-      "status": "状态",
-      "description": "描述",
-      "createTime": "创建时间",
-      "creator": "创建者",
-      "actions": "操作"
-    },
-    "placeholder": {
-      "title": "请输入名称",
-      "description": "请输入描述",
-      "searchText": "请您输入..."
-    },
-    "dir": {
-      "root": "根目录",
-      "testing": "测试集",
-      "validation": "验证集",
-      "training": "训练集"
-    }
-  }
-}
+};
