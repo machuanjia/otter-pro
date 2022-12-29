ad8dac12e8bcaf944b484fda5be910f9ad85f5ca
machuanjia <yanshi0429@126.com>
2022-12-29 16:42:33 +0800

    fix: update otter field row

--- src/components/otter-field-row/index.tsx
+++ src/components/otter-field-row/index.tsx
@@ -1,17 +1,19 @@
-import React from "react"
+import React from 'react';
 import PropTypes from 'prop-types';
 
 type IProps = {
   labelClassName?: string;
   title: string;
   children: PropTypes.ReactNodeLike;
-  [propName:string]:any;
+  [propName: string]: any;
 };
-const OtterFieldRow = ({ title, children, labelClassName='', ...args }: IProps) => {
-  return <div className="flex flex-row mb-3" {...args}>
-    <div className={`${labelClassName} text-weak mr-7`}>{title}</div>
-    <div>{children}</div>
-  </div>
-}
+const OtterFieldRow = ({ title, children, labelClassName = '', ...args }: IProps) => {
+  return (
+    <div className="flex flex-row mb-2" {...args}>
+      <div className={`${labelClassName} text-weak mr-7`}>{title}</div>
+      <div>{children}</div>
+    </div>
+  );
+};
 
-export default OtterFieldRow
+export default OtterFieldRow;
