aa39d9c7da0a02477fd0fa34f4f53ec960393b4d
machuanjia <yanshi0429@126.com>
2023-01-05 16:38:35 +0800

    fix: change

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -2,17 +2,17 @@ import i18n from '../i18n';
 
 export const DATASET_TYPES = {
   REAL: {
-    label: '真实数据',
+    label: i18n.t('common.dataType.real'),
     value: 'DATASET_TYPE_REAL',
     enumValue: 1,
   },
   SYNTHETIC: {
-    label: '合成数据',
+    label: i18n.t('common.dataType.synthesis'),
     value: 'DATASET_TYPE_SYNTHETIC',
     enumValue: 2,
   },
   PRETRAINED: {
-    label: '预训练模型',
+    label: i18n.t('common.dataType.pretraining'),
     value: 'DATASET_TYPE_PRETRAINED',
     enumValue: 3,
   },
